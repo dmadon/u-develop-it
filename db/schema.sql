cca21561380c8cd0d29ac3586cbc926b34936350
dmadon <deanna.madon@gmail.com>
2022-08-09 22:52:51 -0500

    add join statements to api routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,20 @@
-CREATE TABLE candidates(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
-);
+-- Note: candidates table must be dropped FIRST because its foreign key depends on the existence of the parties table
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 
+-- Note: parties table must be created before the candidates table because the candidates table depends on the existence of the parties table's primary key
 CREATE TABLE parties(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
+
+CREATE TABLE candidates(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
