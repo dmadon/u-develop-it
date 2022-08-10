c0204994640de09778d741afa3bbda3cd18ed379
dmadon <deanna.madon@gmail.com>
2022-08-10 18:42:46 -0500

    modularized routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 -- Note: candidates table must be dropped FIRST because its foreign key depends on the existence of the parties table
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 -- Note: parties table must be created before the candidates table because the candidates table depends on the existence of the parties table's primary key
 CREATE TABLE parties(
@@ -16,5 +17,13 @@ CREATE TABLE candidates(
     party_id INTEGER,
     industry_connected BOOLEAN NOT NULL,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE voters(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
