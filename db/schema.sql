26fb5efe0b808592705fa7ca245eda0c671bab2d
dmadon <deanna.madon@gmail.com>
2022-08-10 20:38:15 -0500

    create votes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,6 @@
--- Note: candidates table must be dropped FIRST because its foreign key depends on the existence of the parties table
+-- Note: votes table must be dropped BEFORE candidates and voters because it depends on both of those tables' primary keys
+DROP TABLE IF EXISTS votes;
+-- Note: candidates table must be dropped BEFORE parties because its foreign key depends on the existence of the parties table's primary key
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
@@ -25,5 +27,16 @@ CREATE TABLE voters(
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Note: votes table must be created after the candidates and voters tables because votes table depends on the existence of those tables' primary keys
+CREATE TABLE votes(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    voter_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id),
+    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
 
