60b64d89467f768447bb7d79289bf48c64f029c1
dmadon <deanna.madon@gmail.com>
2022-08-10 20:03:01 -0500

    added voter routes

--- server.js
+++ server.js
@@ -17,11 +17,13 @@ app.use('/api', apiRoutes);
 app.use((req,res) => {
     res.status(404).end();
 });
-db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected');
+
+
+// db.connect(err => {
+//     if (err) throw err;
+//     console.log('Database connected');
 
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
-    });     
+//     });     
 })
